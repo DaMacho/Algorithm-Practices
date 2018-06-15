aa253883c790f4a0df8d7290ad61836e949f3843
DaMacho <po4865@gmail.com>
2018-06-16 00:30:03 +0900

    added

--- Programmers/2018 서머코딩/WordsEndtoend.py
+++ Programmers/2018 서머코딩/WordsEndtoend.py
@@ -1,12 +1,10 @@
 def solution(n, words):
     wrong = 0
     for i in range(1, len(words)):
-        if words[i-1][-1] != words[i][0]:
+        if words[i-1][-1] != words[i][0] or words[i] in words[:i]:
             wrong = i+1
             break
-        elif words[i] in words[:i]:
-            wrong = i+1
-            break
+
     if wrong == 0:
         return [0,0]
     if wrong%n == 0:
